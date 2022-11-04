f848adf97fef2ee77af78b40cb8026f57faa3746
Rostik <rostyslav.kostiuk@ucu,edu>
2022-11-04 14:22:55 +0200

    initialized a project

---
+++ src/test/java/com/example/flowerStoreSite/FlowerStoreSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowerStoreSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerStoreSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
